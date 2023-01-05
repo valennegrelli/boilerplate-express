2f2bb9f01a0328cc5bd81fbf1d5c04662dd62855
valennegrelli02 <valentin.negrelli@mi.unc.edu.ar>
2023-01-05 14:58:42 -0300

    Ex3 Serve HTML page in get at /

--- myApp.js
+++ myApp.js
@@ -1,4 +1,12 @@
 let express = require("express");
 let app = express();
+console.log("Hello World");
+
+function server(req, res) {
+  absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+}
+
+app.get("/", server);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -30,7 +30,6 @@ if (!process.env.DISABLE_XORIGIN) {
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
   bGround.log("Node is listening on port " + port + "...");
-  bGround.log("Hello World");
 });
 
 /******************************************************
