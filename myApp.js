2685b86f1de0f0686cc272975d8cd133671cbe6a
valennegrelli02 <valentin.negrelli@mi.unc.edu.ar>
2023-01-05 15:19:40 -0300

    Ex4 Adding style to static file through middleware

--- myApp.js
+++ myApp.js
@@ -3,10 +3,13 @@ let app = express();
 console.log("Hello World");
 
 function server(req, res) {
-  absolutePath = __dirname + "/views/index.html";
+  const absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
 }
 
-app.get("/", server);
+app.get("/", server); //Serves HTML page
+
+const cssStyle = __dirname + "/public";
+app.use("/public", express.static(cssStyle));
 
 module.exports = app;
